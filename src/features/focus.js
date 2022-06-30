489778aa0340434c224162a24d8c4d9426b3f91b
Tiago <ottiagos@gmail.com>
2022-06-29 23:49:38 -0300

    add state fpr  subject storage

--- src/features/focus.js
+++ src/features/focus.js
@@ -1,27 +1,33 @@
-import React from "react";
-import { View, Text, StyleSheet} from 'react-native';
+import React, { usestate } from "react";
+import { View, Text, StyleSheet } from 'react-native';
 import { TextInput } from "react-native-paper";
 import { colors } from "../utils/colors";
 
-export const Focus = () => (
-    <View style={styles.container}>
-        <View style={styles.inputContainer}>
-        <TextInput 
-            label="What would you like to focus on?"
-        />
+export const Focus = () => {
+    const [subject, setSubject] = usestate(null);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    label="What would you like to focus on?"
+                    onChangeText={(val) => setSubject(val)}
+                    value={subject}
+                />
+            </View>
         </View>
-    </View>
-);
+    );
+}
 
 const styles = StyleSheet.create({
-    container : {
+    container: {
         flex: 1
     },
-    inputContainer : {
+    inputContainer: {
         justifyContent: 'flex-start',
         padding: 25
-    }, 
-    text : {
-        color : colors.white
+    },
+    text: {
+        color: colors.white
     }
 });
